4806dcdfa3166600c3192f172966d6dd8885053f
Danilo Noccioli <noccioli.danilo.lavoro@gmail.com>
2026-08-27 20:06:52 +0200

    creazione del category per vedere tutte le categorie

---
+++ resources/views/posts/category.blade.php
@@ -0,0 +1,30 @@
+<x-layouts.app>
+    <div class="container">
+        <div class="row py-5 justify-content-center align-items-center text-center">
+            <div class="col-12 pt-5">
+                <h1 class="display-3 fw-bold">
+                    Articoli della categoria
+                    <span class="fst-italic text-primary">{{ $category->name }}</span>
+                </h1>
+                <p class="text-muted fs-5 mt-2">{{ $postsByCategory->count() }} articoli trovati</p>
+            </div>
+        </div>
+
+        <div class="row g-4 justify-content-center py-4">
+            @forelse ($postsByCategory as $post)
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                    <x-card :post="$post" />
+                </div>
+            @empty
+                <div class="col-12 text-center py-5">
+                    <h3 class="text-muted">Non sono ancora stati creati articoli per questa categoria!</h3>
+                    @auth
+                        <a href="{{ route('post.create') }}" class="btn btn-primary btn-lg my-4">
+                            Pubblica un articolo
+                        </a>
+                    @endauth
+                </div>
+            @endforelse
+        </div>
+    </div>
+</x-layouts.app>
