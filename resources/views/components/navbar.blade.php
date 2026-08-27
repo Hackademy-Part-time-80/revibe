94b368f4499abc26ef51fb497056f698e03006d7
Danilo Noccioli <noccioli.danilo.lavoro@gmail.com>
2026-08-27 20:57:00 +0200

    aggiunta del logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
     <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="{{ route('homepage') }}">ReVibe</a>
+        <a class="navbar-brand d-flex align-items-center" href="{{ route('homepage') }}">
+    <img src="{{ Vite::asset('resources/images/revibe-logo.svg') }}" alt="ReVibe" height="36">
+</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
