530659b5dbb2ffef8e63c4772b4231039453e070
Giuseppe_Giannelli <peppegiannelli@hotmail.com>
2026-08-27 20:33:34 +0200

    update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    use HasFactory;
     protected $fillable = ['title', 'price', 'description', 'user_id', 'category_id'];
 
     public function category()
