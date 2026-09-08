f9f721af2167afa205b389fd4e8ba8c8fbe25fb7
Danilo Noccioli <noccioli.danilo.lavoro@gmail.com>
2026-09-08 21:10:33 +0200

    user story 8 completata

--- app/Livewire/PostCreate.php
+++ app/Livewire/PostCreate.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use App\Jobs\RemoveFaces;
 
 
 class PostCreate extends Component
@@ -66,7 +67,11 @@ class PostCreate extends Component
             foreach ($this->images as $image) {
                 $newFileName = "post/{$this->post->id}";
                 $newImage = $this->post->images()->create(['path' => $image->store($newFileName, 'public')]);
-                dispatch(new ResizeImage($newImage->path, 300, 300));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 300, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+                ])->dispatch($newImage->id);
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
