b5cb44fd8d8a4ab734ce1f6963d4ee26c083d0bc
SamuelRenno <samuelrenno423@gmail.com>
2026-09-09 19:25:14 +0200

    .

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -19,7 +19,7 @@ class ResizeImage implements ShouldQueue
     private $w, $h, $fileName, $path;
     public function __construct($filePath, $w, $h)
     {
-        $this->path = dirName($filePath);
+        $this->path = dirname($filePath);
         $this->fileName = basename($filePath);
         $this->w = $w;
         $this->h = $h;
@@ -35,7 +35,10 @@ class ResizeImage implements ShouldQueue
         $srcPath = storage_path().'/app/public' . $this->path . '/' . $this->fileName; 
         $destPath = storage_path().'/app/public' . $this->path . "/crop_{$w}x{$h}" . $this->fileName;
 
-        Image::useImageDriver(ImageDriver::Gd)->load($srcPath)->crop($w,$h, CropPosition::Center)->watermark(base_path('resources/images/watermark.svg'),
+        Image::useImageDriver(ImageDriver::Gd)->load($srcPath)
+        ->crop($w,$h, CropPosition::Center)
+        ->watermark(
+            base_path('resources/images/watermark.png'),
         paddingX: 5,
         paddingY: 5,
         width: 50,
