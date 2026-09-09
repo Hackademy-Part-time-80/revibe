8f6a66652fc730abbd1e4625f682b23ea33d8db1
domenico squadrito <domenico.squadritosp@gmail.com>
2026-09-09 18:23:22 +0200

    aggiunta google vision

--- app/Livewire/PostCreate.php
+++ app/Livewire/PostCreate.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Jobs\GoogleVisionSafeSearch;
 use App\Jobs\ResizeImage;
 use App\Models\Category;
 use App\Models\Post;
@@ -53,9 +54,9 @@ class PostCreate extends Component
     public function postStore()
     {
         $this->validate();
-        
 
-        $this->post = Post::create([
+
+        $post = Post::create([
             'title' => $this->title,
             'price' => $this->price,
             'description' => $this->description,
@@ -70,7 +71,6 @@ class PostCreate extends Component
                 RemoveFaces::withChain([
                     new ResizeImage($newImage->path, 300, 300),
                     new GoogleVisionSafeSearch($newImage->id),
-                    new GoogleVisionLabelImage($newImage->id)
                 ])->dispatch($newImage->id);
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
