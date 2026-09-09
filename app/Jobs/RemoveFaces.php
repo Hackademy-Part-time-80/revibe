b1b60ddc0af99a3165c01b6c07f4e2edd9c93802
Danilo Noccioli <noccioli.danilo.lavoro@gmail.com>
2026-09-09 19:51:03 +0200

    fix user story 8

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -2,23 +2,29 @@
 
 namespace App\Jobs;
 
+use App\Models\Image;
+use Google\Cloud\Vision\V1\AnnotateImageRequest;
+use Google\Cloud\Vision\V1\BatchAnnotateImagesRequest;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+use Google\Cloud\Vision\V1\Feature;
+use Google\Cloud\Vision\V1\Feature\Type;
+use Google\Cloud\Vision\V1\Image as VisionImage;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Spatie\Image\Enums\Fit;
-use App\Models\Image;
-use Spatie\Image\Image as SpatieImage;
-use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+use Spatie\Image\Enums\ImageDriver;
 use Spatie\Image\Enums\AlignPosition;
+use Spatie\Image\Enums\Fit;
+use Spatie\Image\Image as SpatieImage;
 
 class RemoveFaces implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
-    private $post_image_id;
+    use Queueable;
 
     /**
      * Create a new job instance.
      */
+    private $post_image_id;
+
     public function __construct($post_image_id)
     {
         $this->post_image_id = $post_image_id;
@@ -34,31 +40,42 @@ class RemoveFaces implements ShouldQueue
             return;
         }
 
-        $srcPath = storage_path('app/public/' . $i->path);
-        $image = file_get_contents($srcPath);
-
+        $src = storage_path('app/public/' . $i->path);
+        $image = file_get_contents($src);
         putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credential.json'));
 
-        $imageAnnotator = new ImageAnnotatorClient();
-        $response = $imageAnnotator->faceDetection($image);
+        $googleVisionClient = new ImageAnnotatorClient();
+        $google_image = new VisionImage([
+            'content' => $image]);
+
+        $googleFeature = new Feature();
+        $googleFeature->setType(Type::FACE_DETECTION);
+
+        $request = new AnnotateImageRequest();
+        $request->setImage($google_image);
+        $request->setFeatures([$googleFeature]);
+
+        $batchRequest = new BatchAnnotateImagesRequest();
+        $batchRequest->setRequests([$request]);
+
+        $responseBatch = $googleVisionClient->batchAnnotateImages($batchRequest);
+        $response = $responseBatch->getResponses()[0];
         $faces = $response->getFaceAnnotations();
 
         foreach ($faces as $face) {
             $vertices = $face->getBoundingPoly()->getVertices();
-
             $bounds = [];
-
-            foreach ($vertices as $vertex) {
+            foreach($vertices as $vertex) {
                 $bounds[] = [$vertex->getX(), $vertex->getY()];
             }
 
             $w = $bounds[2][0] - $bounds[0][0];
             $h = $bounds[2][1] - $bounds[0][1];
 
-            $image = SpatieImage::load($srcPath);
+            $image = SpatieImage::useImageDriver(ImageDriver::Gd)->load($src);
 
             $image->watermark(
-                base_path('resources/img/faces.png'),
+                base_path('resources/images/face.png'),
                 AlignPosition::TopLeft,
                 paddingX: $bounds[0][0],
                 paddingY: $bounds[0][1],
@@ -66,10 +83,10 @@ class RemoveFaces implements ShouldQueue
                 height: $h,
                 fit: Fit::Stretch
             );
-
-            $image->save($srcPath);
+            $image->save($src);
         }
+        $googleVisionClient->close();
 
-        $imageAnnotator->close();
+       
     }
 }
